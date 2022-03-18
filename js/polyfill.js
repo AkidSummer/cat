7062ba6b6ebbe4460a6ed74fc407007803d9bdd5
Akie <96067567+AkidSummer@users.noreply.github.com>
2022-03-18 19:56:58 +0800

    Add files via upload

---
+++ js/polyfill.js
@@ -0,0 +1,3 @@
+// build time:Fri Mar 18 2022 19:53:02 GMT+0800 (中国标准时间)
+(function(o){}).call("object"===typeof window&&window||"object"===typeof self&&self||"object"===typeof global&&global||{});
+//rebuild by hrmmi 
